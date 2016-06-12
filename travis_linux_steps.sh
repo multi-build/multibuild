db8656e71052c1d50260298a96c7e8eba0c62f00
Matthew Brett <matthew.brett@gmail.com>
2016-06-12 12:23:18 -0700

    Refactor unicode width logic

--- travis_linux_steps.sh
+++ travis_linux_steps.sh
@@ -5,6 +5,7 @@ set -e
 ROOT_DIR=$(dirname "${BASH_SOURCE[0]}")
 UTIL_DIR=${UTIL_DIR:-${ROOT_DIR}/manylinux}
 BUILD_SCRIPT=${BUILD_SCRIPT:-/io/$UTIL_DIR/build_package.sh}
+UNICODE_WIDTHS=${UNICODE_WIDTHS:-32}
 
 function before_install {
     virtualenv --python=python venv
@@ -29,6 +30,25 @@ function build_wheels {
     build_plat_wheels x86_64
 }
 
+function valid_unicode_widths {
+    local py_ver=${1:-$TRAVIS_PYTHON_VERSION}
+    local ok_widths=""
+    if [ "${py_ver:0:1}" == 2 ]; then local py2=1; fi
+    for width in ${@:2}; do
+        if [ "$width" == 32 ]; then
+            ok_widths="$ok_widths 32"
+        elif [ "$width" == 16 ]; then
+            if [ -n "$py2" ]; then
+                ok_widths="$ok_widths 16"
+            fi
+        else
+            echo "Invalid unicode width $width"
+            exit 1
+        fi
+    done
+    echo $ok_widths
+}
+
 function build_plat_wheels {
     # Builds wheels
     #
@@ -44,10 +64,11 @@ function build_plat_wheels {
     local docker_image=quay.io/pypa/manylinux1_$plat
     docker pull $docker_image
     if [ "$plat" == "i686" ]; then local intro_cmd=linux32; fi
+    local widths=$(valid_unicode_widths $TRAVIS_PYTHON_VERSION $UNICODE_WIDTHS)
     docker run --rm \
         -e UTIL_DIR="$UTIL_DIR" \
         -e PYTHON_VERSION="$TRAVIS_PYTHON_VERSION" \
-        -e UNICODE_WIDTHS="$UNICODE_WIDTHS" \
+        -e UNICODE_WIDTHS="$widths" \
         -e BUILD_DEPENDS="$BUILD_DEPENDS" \
         -e BUILD_COMMIT="$BUILD_COMMIT" \
         -e BUILD_PRE_SCRIPT="$BUILD_PRE_SCRIPT" \
