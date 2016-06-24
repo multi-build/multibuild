14ee2c233ac3f409f2cff0c775df2f5528176725
Matthew Brett <matthew.brett@gmail.com>
2016-06-24 11:44:46 -0700

    Fix test for presence of function

--- common_utils.sh
+++ common_utils.sh
@@ -113,7 +113,7 @@ function build_wheel_cmd {
     local repo_dir=${2:-$REPO_DIR}
     [ -z "$repo_dir" ] && echo "repo_dir not defined" && exit 1
     local wheelhouse=$(abspath ${WHEEL_SDIR:-wheelhouse})
-    if [ -n $(is_function "pre_build") ]; then pre_build; fi
+    if [ -n "$(is_function "pre_build")" ]; then pre_build; fi
     if [ -n "$BUILD_DEPENDS" ]; then
         pip install $(pip_opts) $BUILD_DEPENDS
     fi
