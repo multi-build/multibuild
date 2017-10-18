b5c75ad96edbb93b3ed33a5646a1e0969ce9b4fb
xoviat <xoviat@users.noreply.github.com>
2017-10-17 22:53:15 -0500

    FIX: spelling

--- tests/test_library_builders.sh
+++ tests/test_library_builders.sh
@@ -4,11 +4,11 @@ export BUILD_PREFIX="${PWD}/builds"
 rm_mkdir $BUILD_PREFIX
 source library_builders.sh
 
-function surpress {
+function suppress {
     # Suppress the output of a bash command unless it fails
-    /bin/rm --force /tmp/surpress.out 2> /dev/null
-    $* 2>&1 > /tmp/surpress.out || cat /tmp/surpress.out
-    /bin/rm /tmp/surpress.out
+    /bin/rm --force /tmp/suppress.out 2> /dev/null
+    $* 2>&1 > /tmp/suppress.out || cat /tmp/suppress.out
+    /bin/rm /tmp/suppress.out
 }
 
 suppress build_openssl
