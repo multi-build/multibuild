e5dd9bf23dca813e0eb7d50579dbce5dc807c212
xoviat <xoviat@users.noreply.github.com>
2017-10-17 22:49:23 -0500

    TST: add some simple tests

--- tests/test_library_builders.sh
+++ tests/test_library_builders.sh
@@ -4,4 +4,13 @@ export BUILD_PREFIX="${PWD}/builds"
 rm_mkdir $BUILD_PREFIX
 source library_builders.sh
 
-build_openssl
+function surpress {
+    # Suppress the output of a bash command unless it fails
+    /bin/rm --force /tmp/surpress.out 2> /dev/null
+    $* 2>&1 > /tmp/surpress.out || cat /tmp/surpress.out
+    /bin/rm /tmp/surpress.out
+}
+
+suppress build_openssl
+suppress build_libwebp
+suppress build_szip
