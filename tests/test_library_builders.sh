6f1885d8fabb5d70e362045f1380b81776e2aed4
xoviat <xoviat@users.noreply.github.com>
2017-10-22 13:36:55 -0500

    FIX: try to figure out why this doesn't work on OSX

--- tests/test_library_builders.sh
+++ tests/test_library_builders.sh
@@ -4,6 +4,8 @@ export BUILD_PREFIX="${PWD}/builds"
 rm_mkdir $BUILD_PREFIX
 source library_builders.sh
 
+set -e -x
+
 function suppress {
     # Suppress the output of a bash command unless it fails
     rm --force $HOME/suppress.out 2> /dev/null
