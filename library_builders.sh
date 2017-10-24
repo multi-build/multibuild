d7e813cf983df95f53e6bf35e8ad8f3b20779e41
xoviat <xoviat@users.noreply.github.com>
2017-10-23 22:49:05 -0500

    ENH: allow omission of .tar.gz

--- library_builders.sh
+++ library_builders.sh
@@ -57,6 +57,10 @@ function build_simple {
     local url=$3
     local ext=${4:-tar.gz}
     local configure_args=${@:5}
+    if [[ $ext == --* ]]; then
+        configure_args="$ext $configure_args"
+        ext=".tar.gz"
+    fi
     if [ -e "${name}-stamp" ]; then
         return
     fi
@@ -177,7 +181,7 @@ function build_libwebp {
     build_tiff
     build_giflib
     build_simple libwebp $LIBWEBP_VERSION \
-        https://storage.googleapis.com/downloads.webmproject.org/releases/webp/ tar.gz \
+        https://storage.googleapis.com/downloads.webmproject.org/releases/webp/ \
         --enable-libwebpmux --enable-libwebpdemux 
 }
 
@@ -195,7 +199,7 @@ function build_szip {
     # Build szip without encoding (patent restrictions)
     build_zlib
     build_simple szip $SZIP_VERSION \
-        https://www.hdfgroup.org/ftp/lib-external/szip/ tar.gz \
+        https://www.hdfgroup.org/ftp/lib-external/szip/ \
         --enable-encoding=no
 }
 
