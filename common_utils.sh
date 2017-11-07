b79fec12a93b0d5002107f330abcd7e4bc2ef3b5
xoviat <xoviat@users.noreply.github.com>
2017-11-07 15:46:47 -0600

    [common_utils] add progress bar code

--- common_utils.sh
+++ common_utils.sh
@@ -353,3 +353,56 @@ retry () {
     }
     return 0
 }
+
+MULTIBULD_PROGRESS_BAR_PID=0
+
+function start_progress {
+    run_progress &
+    MULTIBULD_PROGRESS_BAR_PID=$!
+}
+
+function stop_progress {
+    kill $MULTIBULD_PROGRESS_BAR_PID >/dev/null 2>&1
+}
+
+function run_progress {
+    # maximal_size_of_bar speed_in_seconds first_delimiter fill_chars last_delimiter
+    MAX=${1:-11}
+    TIME="${2:-0.08}"
+    TL="${3:-[}"
+    S="${4:-#####}"
+    TR="${5:-]}"
+
+    while true; do
+        R=0
+        while [ $R -lt $MAX ]; do 
+            RSP=$(($MAX - $R ))
+            if [ $RSP -gt $MAX ]; then RSP=$MAX ; fi 
+            LSP=$(($MAX - ${RSP}))
+            echo -n "$TL"
+            for l in $(seq 1 $LSP); do
+                echo -n " "
+            done
+            echo -n $S
+            for r in $(seq 1 $RSP); do
+                echo -n " "
+            done; echo -ne "$TR\r"
+            sleep $TIME ; ((R++))
+        done
+        while [ $R -ne 0 ]; do
+            RSP=$(($MAX - $R ))
+            if [ $RSP -ge $MAX ]; then RSP=$MAX ; fi 
+            LSP=$(($R + 0 )) 
+            if [ $LSP -lt 0 ]; then LSP=0 ; fi 
+            echo -n "$TL"
+            for l in $(seq 1 $R); do
+                echo -n " "
+            done
+            echo -n $S
+            for r in $(seq 1 $RSP); do
+                echo -n " "
+            done; echo -ne "$TR\r"
+            sleep $TIME; ((R--))
+        done
+    done
+}
