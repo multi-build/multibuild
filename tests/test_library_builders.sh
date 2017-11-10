a8f873d6f7c5003bcf43f0c0268b7216a6a5d76d
xoviat <xoviat@users.noreply.github.com>
2017-11-10 17:19:01 -0600

    [tst:libraries] add start and stop spinner

--- tests/test_library_builders.sh
+++ tests/test_library_builders.sh
@@ -8,9 +8,13 @@ source library_builders.sh
 
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then brew update; fi
 
+start_spinner
+
 suppress build_openssl
 suppress build_openblas
 suppress build_libwebp
 suppress build_szip
 suppress build_swig
 suppress build_github fredrik-johansson/arb 2.11.1
+
+stop_spinner
