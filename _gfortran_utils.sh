bfa4b64adf0bd3f46d8da7948641ec779ff43ca8
xoviat <xoviat@users.noreply.github.com>
2017-11-29 14:25:29 -0600

    Add gfortran utils

---
+++ _gfortran_utils.sh
@@ -0,0 +1,23 @@
+# Stripped-down version of bash utilities for use with gfortran
+
+ARCHIVE_SDIR="${ARCHIVE_SDIR:-archives}"
+GF_UTIL_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+function _mb_get_gf_lib_for_suf {
+    local suffix=$1
+    local prefix=$2
+    local plat=${3:-$PLAT}
+    local uname=${4:-$(uname)}
+    if [ -z "$prefix" ]; then echo Prefix not defined; exit 1; fi
+    local fname="$prefix-${uname}-${plat}${suffix}.tar.gz"
+    local out_fname="${ARCHIVE_SDIR}/$fname"
+    if [ ! -e "$out_fname" ]; then
+        curl -L "${GF_LIB_URL}/$fname" > $out_fname || (echo "Fetch failed"; exit 1)
+    fi
+    echo "$out_fname"
+}
+
+function _mb_get_gf_lib {
+    # Get library with no suffix
+    _mb_get_gf_lib_for_suf "" $@
+}
