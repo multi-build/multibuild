15e4d34356700c2f169fd1f7b4d534950ba9d07e
Andrew Murray <radarhere@users.noreply.github.com>
2018-12-02 20:36:19 +1100

    Updated libpng to 1.6.36

--- tests/test_library_builders.sh
+++ tests/test_library_builders.sh
@@ -18,7 +18,7 @@ suppress build_swig
 # Run here just for the output, even though they fail.
 (set +e ;
     build_github fredrik-johansson/arb 2.15.0 ;
-    build_github glennrp/libpng v1.6.35 ;
+    build_github glennrp/libpng v1.6.36 ;
     build_github wbhart/mpir mpir-3.0.0
     )
 suppress build_flex
