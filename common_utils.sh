d505afbae7dc2580745b8237a93eb02d3f5fc7ef
Andrew Murray <radarhere@users.noreply.github.com>
2018-12-06 19:46:27 +1100

    Install TEST_DEPENDS packages one line a time

--- common_utils.sh
+++ common_utils.sh
@@ -323,9 +323,9 @@ function install_wheel {
     #     MANYLINUX_URL (optional, default "") (via pip_opts function)
     local wheelhouse=$(abspath ${WHEEL_SDIR:-wheelhouse})
     if [ -n "$TEST_DEPENDS" ]; then
-        for TEST_DEPENDENCY in $TEST_DEPENDS; do
+        while read TEST_DEPENDENCY; do
             pip install $(pip_opts) $@ $TEST_DEPENDENCY
-        done
+        done <<< "$TEST_DEPENDS"
     fi
     # Install compatible wheel
     pip install $(pip_opts) $@ \
