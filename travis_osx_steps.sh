ab2865741b4d3766c46741746e7287cc5f1743fb
Rob Buckley <20515024+robbuckley@users.noreply.github.com>
2019-02-12 00:12:06 +0000

    remove MACPYTHON_DEFAULT_OSX logic from test_multibuild.sh and travis_osx_steps.sh

--- travis_osx_steps.sh
+++ travis_osx_steps.sh
@@ -9,8 +9,6 @@ ENV_VARS_PATH=${ENV_VARS_PATH:-env_vars.sh}
 
 # These load common_utils.sh
 source $MULTIBUILD_DIR/osx_utils.sh
-# osx_utils defines MACPYTHON_DEFAULT_OSX
-MB_PYTHON_OSX_VER=${$MB_PYTHON_OSX_VER:-$MACPYTHON_DEFAULT_OSX}
 if [ -r "$ENV_VARS_PATH" ]; then source "$ENV_VARS_PATH"; fi
 source $MULTIBUILD_DIR/configure_build.sh
 source $MULTIBUILD_DIR/library_builders.sh
@@ -23,7 +21,7 @@ function before_install {
     brew cask uninstall oclint || true    
     export CC=clang
     export CXX=clang++
-    get_macpython_environment $MB_PYTHON_VERSION $MB_PYTHON_OSX_VER venv
+    get_macpython_environment $MB_PYTHON_VERSION ${MB_PYTHON_OSX_VER:-""} venv
     source venv/bin/activate
     pip install --upgrade pip wheel
 }
