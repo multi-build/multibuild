e06e8295f26fbc635cfeee75a430aa166ca1e8f9
Andrew Murray <radarhere@users.noreply.github.com>
2020-12-22 12:07:49 +1100

    Updated Python versions

--- osx_utils.sh
+++ osx_utils.sh
@@ -11,14 +11,14 @@ MACPYTHON_PY_PREFIX=/Library/Frameworks/Python.framework/Versions
 GET_PIP_URL=https://bootstrap.pypa.io/get-pip.py
 WORKING_SDIR=working
 
-# As of 8 December 2020 - latest Python of each version with binary download
+# As of 22 December 2020 - latest Python of each version with binary download
 # available.
 # See: https://www.python.org/downloads/mac-osx/
 LATEST_2p7=2.7.18
 LATEST_3p5=3.5.4
 LATEST_3p6=3.6.8
 LATEST_3p7=3.7.9
-LATEST_3p8=3.8.6
+LATEST_3p8=3.8.7
 LATEST_3p9=3.9.1
 
 
