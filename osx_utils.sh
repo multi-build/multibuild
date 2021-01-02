12426f0a873305adc59d9c37135caaddc11e6e3a
robbuckley <20515024+robbuckley@users.noreply.github.com>
2021-01-02 17:08:39 +0000

    Update osx_utils.sh
    Co-authored-by: Andrew Murray <3112309+radarhere@users.noreply.github.com>

--- osx_utils.sh
+++ osx_utils.sh
@@ -292,7 +292,7 @@ function install_mac_cpython {
     local inst_path=$DOWNLOADS_SDIR/$py_inst
     local retval=""
     mkdir -p $DOWNLOADS_SDIR
-    # exit early on curl errors, but dont let it exit the shell
+    # exit early on curl errors, but don't let it exit the shell
     cmd_notexit curl -f $MACPYTHON_URL/$py_stripped/${py_inst} > $inst_path || retval=$?
     if [ ${retval:-0} -ne 0 ]; then
       echo "Python download failed! Check ${py_inst} exists on the server."
