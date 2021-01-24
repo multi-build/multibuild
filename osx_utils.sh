ef449193e985e5909da9e8b0b5dd12091d5de8c6
Isuru Fernando <isuruf@gmail.com>
2021-01-24 12:59:27 -0600

    set ARCHFLAGS too

--- osx_utils.sh
+++ osx_utils.sh
@@ -464,7 +464,9 @@ function macos_arm64_cross_build_setup {
 function macos_arm64_native_build_setup {
     # Setup native build for single arch arm_64 wheels
     export PLAT="arm64"
+    # We don't want universal2 builds and only want an arm64 build
     export _PYTHON_HOST_PLATFORM="macosx-11.0-arm64"
+    export ARCHFLAGS+="-arch arm64"
     $@
 }
 
