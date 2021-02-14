4ce0414c258c21d98b8d63fd27b0eb756e12f938
Isuru Fernando <isuruf@gmail.com>
2021-02-14 11:28:19 -0600

    Check that sdk is >=11 after selecting too

--- travis_osx_steps.sh
+++ travis_osx_steps.sh
@@ -7,7 +7,8 @@ if [ "$PLAT" == "arm64" ] || [ "$PLAT" == "universal2" ]; then
     latestXcode=$(ls /Applications | grep Xcode[_0-9\.]*\.app | sort -V | tail -n 1)
     if ([ "$GITHUB_WORKFLOW" != "" ] || [ "$PIPELINE_WORKSPACE" != "" ]) && [ $latestXcode ]; then
       sudo xcode-select -switch /Applications/$latestXcode.app
-    else
+    fi
+    if [[ "$(xcrun -show-sdk-version)" == 10.* ]]; then
       echo "Need SDK>=11 for arm64 builds. Please run xcode-select to select a newer SDK"
       exit 1
     fi
