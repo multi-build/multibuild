8a9333e307fa3f24db2089a66f25e38a8272c261
Christian Clauss <cclauss@me.com>
2024-10-23 13:42:38 +0200

    Format and lint Python code with ruff (#533)
    * Format and lint Python code with ruff
    
    * uff check --output-format=github --select=ALL --ignore=D203,D212,Q000

--- supported_wheels.py
+++ supported_wheels.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python
-"""  Filter out wheel filenames not supported on this platform
-"""
-from __future__ import print_function
+"""Filter out wheel filenames not supported on this platform."""
+
+from __future__ import annotations
 
 import sys
 from os.path import basename
+from typing import TYPE_CHECKING
 
 from packaging.tags import sys_tags
 
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+
 try:
-    from wheel.install import WHEEL_INFO_RE as wheel_matcher
+    from wheel.install import WHEEL_INFO_RE as wheel_matcher  # noqa: N811
 except ImportError:  # As of Wheel 0.32.0
     from wheel.wheelfile import WHEEL_INFO_RE
+
     wheel_matcher = WHEEL_INFO_RE.match
 
 
-def tags_for(fname):
-    # Copied from WheelFile code
-    parsed_filename = wheel_matcher(basename(fname))
+def tags_for(fname: str) -> Iterator[tuple[str, str, str]]:
+    """Copied from WheelFile code."""  # noqa: D401
+    parsed_filename = wheel_matcher(basename(fname))  # noqa: PTH119
     tags = parsed_filename.groupdict()
     for pyver in tags['pyver'].split('.'):
         for abi in tags['abi'].split('.'):
@@ -25,14 +30,13 @@ def tags_for(fname):
                 yield (pyver, abi, plat)
 
 
-def main():
-    supported = {
-        (tag.interpreter, tag.abi, tag.platform) for tag in sys_tags()
-    }
+def main() -> None:
+    """Print filenames of all supported wheels."""
+    supported = {(tag.interpreter, tag.abi, tag.platform) for tag in sys_tags()}
     for fname in sys.argv[1:]:
         tags = set(tags_for(fname))
         if supported.intersection(tags):
-            print(fname)
+            print(fname)  # noqa: T201
 
 
 if __name__ == '__main__':
